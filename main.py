5186645ed6102e1f0bcfd48face25e68ea3e984d
VictorJimenez99 <jimenezvictor99@gmail.com>
2021-12-11 16:09:23 -0600

    simple test

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-
-from pysnmp import *
-
-
+from pysnmp.hlapi import *
 
 if __name__ == '__main__':
-    print("start")
+    iterator = getCmd(
+        SnmpEngine(),
+        UsmUserData(userName="admin_snmp", authKeyType="admin", privKey="admin", authProtocol=usmDESPrivProtocol),
+        UdpTransportTarget(('10.1.0.254', 161)),
+        ContextData(),
+        ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0))
+    )
 
+    errorIndication, errorStatus, errorIndex, varBinds = next(iterator)
+
+    if errorIndication:
+        print(f"errorIndication: {errorIndication}")
+
+    elif errorStatus:
+        print('%s at %s' % (errorStatus.prettyPrint(),
+                            errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+
+    else:
+        for varBind in varBinds:
+            print(' = '.join([x.prettyPrint() for x in varBind]))
