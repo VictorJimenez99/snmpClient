2672c4ab65c0270eea7ad7f38640b43068725bfe
VictorJimenez99 <jimenezvictor99@gmail.com>
2021-12-11 20:14:46 -0600

    tests different oid rev2

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 from pysnmp.hlapi import *
 
-if __name__ == '__main__':
+
+def get_oid_value(ip, oid):
     engine = SnmpEngine()
-    user_data=UsmUserData(userName=b"snmp_admin",
-                          authKey=b"qwertyui",
-                          privKey=b"qwertyui",
-                          privProtocol=(1, 3, 6, 1, 6, 3, 10, 1, 2, 2),
-                          authProtocol=(1, 3, 6, 1, 6, 3, 10, 1, 1, 3))
+
+    user_data = UsmUserData(userName=b"snmp_admin",
+                            authKey=b"qwertyui",
+                            privKey=b"qwertyui",
+                            privProtocol=(1, 3, 6, 1, 6, 3, 10, 1, 2, 2),
+                            authProtocol=(1, 3, 6, 1, 6, 3, 10, 1, 1, 3))
     print(user_data)
     iterator = getCmd(
         engine, user_data,
-        UdpTransportTarget(('10.1.0.254', 161)),
+        UdpTransportTarget((ip, 161)),
         ContextData(),
-        ObjectType(ObjectIdentity("1.3.6.1.4.1.9.2.1.3.0"))
+        ObjectType(ObjectIdentity(oid))
     )
+    error_indication, error_status, error_index, var_binds = next(iterator)
+    if error_indication:
+        print(f"errorIndication: {error_indication}")
 
-    errorIndication, errorStatus, errorIndex, varBinds = next(iterator)
-
-    if errorIndication:
-        print(f"errorIndication: {errorIndication}")
-
-    elif errorStatus:
-        print('%s at %s' % (errorStatus.prettyPrint(),
-                            errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
+    elif error_status:
+        print('%s at %s' % (error_status.prettyPrint(),
+                            error_index and var_binds[int(error_index) - 1][0] or '?'))
 
     else:
-        for varBind in varBinds:
+        for varBind in var_binds:
             print(' = '.join([x.prettyPrint() for x in varBind]))
+
+
+if __name__ == '__main__':
+    get_oid_value("10.1.0.254", "1.3.6.1.2.1.2.2.1.11.1")
