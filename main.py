f746fc0adf111fa047448e7a0d25f743d69bcd01
VictorJimenez99 <jimenezvictor99@gmail.com>
2021-12-12 12:43:00 -0600

    getter for different things at once rev3

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ if __name__ == '__main__':
     sys_description = '1.3.6.1.2.1.1.1.0'
     sys_location = '1.3.6.1.2.1.1.6.0'
     sys_contact = '1.3.6.1.2.1.1.4.0'
-    sys_name = "1.3.6.1.4.1.8.2.1.3"
+    sys_name = "1.3.6.1.4.1.9.2.1.3"
 
     get_oid_value("10.1.0.254", sys_name)
     get_oid_value("10.1.0.254", sys_location)
