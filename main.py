15b12c1ad0153f17b1e7d280b0a38235b194ce29
VictorJimenez99 <jimenezvictor99@gmail.com>
2021-12-12 12:04:26 -0600

    setter rev1

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ def get_oid_value(ip, oid):
                             privKey=b"qwertyui",
                             privProtocol=(1, 3, 6, 1, 6, 3, 10, 1, 2, 2),
                             authProtocol=(1, 3, 6, 1, 6, 3, 10, 1, 1, 3))
-    print(user_data)
     iterator = getCmd(
         engine, user_data,
         UdpTransportTarget((ip, 161)),
@@ -29,5 +28,23 @@ def get_oid_value(ip, oid):
             print(' = '.join([x.prettyPrint() for x in varBind]))
 
 
+def set_oid_value(ip, oid, new_value):
+    engine = SnmpEngine()
+
+    user_data = UsmUserData(userName=b"snmp_admin",
+                            authKey=b"qwertyui",
+                            privKey=b"qwertyui",
+                            privProtocol=(1, 3, 6, 1, 6, 3, 10, 1, 2, 2),
+                            authProtocol=(1, 3, 6, 1, 6, 3, 10, 1, 1, 3))
+    setCmd(
+        engine, user_data,
+        UdpTransportTarget((ip, 161)),
+        ContextData(),
+        ObjectType(ObjectIdentity(oid), new_value)
+    )
+
+
 if __name__ == '__main__':
-    get_oid_value("10.1.0.254", "1.3.6.1.2.1.1.5")
+    get_oid_value("10.1.0.254", "1.3.6.1.2.1.1.6.0")
+    set_oid_value("10.1.0.254", "1.3.6.1.2.1.1.6.0", "primer nodo, Lugar1")
+    get_oid_value("10.1.0.254", "1.3.6.1.2.1.1.6.0")
